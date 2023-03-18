4e1a7dfa758e678df08f4a8462922edb0adfc6b1
bebe-Rebecca <126058878+bebe-Rebecca@users.noreply.github.com>
2023-03-18 12:58:01 +0900

    Userモデル単体テスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname,        presence: true       
-  validates :last_name,       presence: true       
-  validates :first_name,      presence: true       
-  validates :last_name_kana,  presence: true  
-  validates :first_name_kana, presence: true  
-  validates :birth_date,      presence: true  
-  
+  with_options presence: true do
+    validates :nickname, :birth_date, :password_confirmation
+    validates :first_name, :last_name, format: { with: /\A[ぁ-んァ-ン一-龥々]/, message: 'is invalid. Input full-width characters' }
+    validates :last_name_kana, :first_name_kana,
+              format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters' }
+  end
+  validates :password, :password_confirmation,
+            format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'is invalid. Include both letters and numbers' }
 end
