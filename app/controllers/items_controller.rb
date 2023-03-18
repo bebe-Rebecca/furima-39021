c60ff88eddf834b8d7acfa5499584689e33b56b1
bebe-Rebecca <126058878+bebe-Rebecca@users.noreply.github.com>
2023-03-18 18:22:20 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,12 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def create
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :condition_id, :shipping_fee_burden_id, :origin_to_prefecture_id, :days_to_ship_id, :price, :image).merge(user_id: current_user.id)
+  end
 end
