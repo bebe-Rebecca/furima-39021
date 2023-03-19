a1be04c188f756673f78873acd503a03e37e03ef
bebe-Rebecca <126058878+bebe-Rebecca@users.noreply.github.com>
2023-03-19 18:55:08 +0900

    ActiveHashを用いたカテゴリー選択等の設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,20 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_fee_burden
+  belongs_to :origin_to_prefecture
+  belongs_to :days_to_ship
 
   validates :name,                    presence: true
   validates :description,             presence: true
-  validates :category_id,             presence: true
-  validates :condition_id,            presence: true
-  validates :shipping_fee_burden_id,  presence: true
-  validates :origin_to_prefecture_id, presence: true
-  validates :days_to_ship_id,         presence: true
+  validates :category_id,             presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :condition_id,            presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_fee_burden_id,  presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :origin_to_prefecture_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :days_to_ship_id,         presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :price,                   presence: true
   validates :image,                   presence: true  
 end
