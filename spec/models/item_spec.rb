ec4268fcaba3cdf468ff20b9c95bb337fa0285c3
bebe-Rebecca <126058878+bebe-Rebecca@users.noreply.github.com>
2023-03-21 15:05:05 +0900

    コードの修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -28,27 +28,27 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Description can't be blank")
       end
       it 'カテゴリーに「---」が選択されている場合は出品できない' do
-        @item.category_id = '---'
+        @item.category_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Category can't be blank")
       end
       it '商品の状態に「---」が選択されている場合は出品できない' do
-        @item.condition_id = '---'
+        @item.condition_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Condition can't be blank")
       end
       it '配送料の負担に「---」が選択されている場合は出品できない' do
-        @item.shipping_fee_burden_id = '---'
+        @item.shipping_fee_burden_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Shipping fee burden can't be blank")
       end
       it '発送元の地域に「---」が選択されている場合は出品できない' do
-        @item.origin_to_prefecture_id = '---'
+        @item.origin_to_prefecture_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Origin to prefecture can't be blank")
       end
       it '発送までの日数に「---」が選択されている場合は出品できない' do
-        @item.days_to_ship_id = '---'
+        @item.days_to_ship_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Days to ship can't be blank")
       end
