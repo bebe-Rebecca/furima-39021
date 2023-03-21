390da72f2b79c192379d9fcdb71ca7c68c566dc9
bebe-Rebecca <126058878+bebe-Rebecca@users.noreply.github.com>
2023-03-21 11:52:34 +0900

    商品出品機能テストコード作成

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,77 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品出品' do
+    context '出品できる場合' do
+      it "全項目の値が存在すれば保存できる" do
+        expect(@item).to be_valid
+      end
+    end
+    context '出品できない場合' do
+      it "画像が添付されていないと保存できない" do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it "商品名がないと保存できない" do
+        @item.name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+      it "商品の説明がないと保存できない" do
+        @item.description = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Description can't be blank")
+      end
+      it "カテゴリーが選択されていないと保存できない" do
+        @item.category_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+      it "商品状態が選択されていないと保存できない" do
+        @item.condition_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition can't be blank")
+      end
+      it "配送料の負担が選択されていないと保存できない" do
+        @item.shipping_fee_burden_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping fee burden can't be blank")
+      end
+      it "発送元の地域が選択されていないと保存できない" do
+        @item.origin_to_prefecture_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Origin to prefecture can't be blank")
+      end
+      it "発送までの日数が選択されていないと保存できない" do
+        @item.days_to_ship_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Days to ship can't be blank")
+      end
+      it "販売価格が空だと保存できない" do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+      it "販売価格は¥300以上でないと保存できない" do
+        @item.price = 299
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+      end
+      it "販売価格は¥9,999,999以下でないと保存できない" do
+        @item.price = 10000000
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+      end
+      it "販売価格は半角数値でないと保存できない" do
+        @item.price = "１００００"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+    end
+  end
 end
