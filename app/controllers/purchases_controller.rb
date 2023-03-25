b5e2588105b12ab5089ba7d59c7f0c2d9cf1e7cc
bebe-Rebecca <126058878+bebe-Rebecca@users.noreply.github.com>
2023-03-25 19:00:33 +0900

    Formオブジェクトを用いた実装

---
+++ app/controllers/purchases_controller.rb
@@ -0,0 +1,25 @@
+class PurchasesController < ApplicationController
+
+  def index
+    @item = Item.find(params[:item_id])
+    @purchase_shipping_address = PurchaseShippingAddress.new
+  end
+
+  def create
+    @item = Item.find(params[:item_id])
+    @purchase_shipping_address = PurchaseShippingAddress.new(purchase_params)
+    if @purchase_shipping_address.valid?
+      @purchase_shipping_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+  
+
+  private
+
+  def purchase_params
+    params.require(:purchase_shipping_address).permit(:postal_code, :origin_to_prefecture_id, :city, :address_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+end
---
+++ app/models/purchase_shipping_address.rb
@@ -0,0 +1,19 @@
+class PurchaseShippingAddress
+  include ActiveModel::Model
+  attr_accessor :postal_code, :origin_to_prefecture_id, :city, :address_number, :building_name, :phone_number, :purchase_id, :user_id, :item_id
+
+  with_options presence: true do
+    validates :user_id
+    validates :item_id
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)"}
+    validates :city
+    validates :address_number
+    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid. Input only number'}
+  end
+  validates :origin_to_prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
+
+  def save
+    purchase = Purchase.create(user_id: user_id, item_id: item_id)
+    ShippingAddress.create(postal_code: postal_code, origin_to_prefecture_id: origin_to_prefecture_id, city: city, address_number: address_number, building_name: building_name, phone_number: phone_number, purchase_id: purchase.id)
+  end
+end
