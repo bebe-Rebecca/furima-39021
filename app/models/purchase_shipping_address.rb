1fc848d0c161e9111ae38221a09e771498092546
bebe-Rebecca <126058878+bebe-Rebecca@users.noreply.github.com>
2023-03-28 19:29:01 +0900

    クレジットカード決済機能の実装

--- app/models/purchase_shipping_address.rb
+++ app/models/purchase_shipping_address.rb
@@ -1,10 +1,11 @@
 class PurchaseShippingAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :origin_to_prefecture_id, :city, :address_number, :building_name, :phone_number, :purchase_id, :user_id, :item_id
+  attr_accessor :postal_code, :origin_to_prefecture_id, :city, :address_number, :building_name, :phone_number, :purchase_id, :user_id, :item_id, :token
 
   with_options presence: true do
     validates :user_id
     validates :item_id
+    validates :token
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)"}
     validates :city
     validates :address_number
